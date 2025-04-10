38c37aedbd6ec1505a95b0b625d7fe61f2d13cdd
mavery735@gmail.com <mavery735@gmail.com>
2025-04-10 02:21:27 -0400

    fixed font not working

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:style=Bold:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Liberation Mono:style=Bold:pixelsize=16";
 static int borderpx = 2;
 
 /*
